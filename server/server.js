f515c43de7d2d9516b761806cbd1d5664830fc2b
damir <damir.say80@gmail.com>
2024-12-06 04:32:54 +0700

    test8

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: clientUrl,
+    origin: [clientUrl, 'http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
