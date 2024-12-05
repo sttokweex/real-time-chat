a3f2955fa29ac78c96ac67b6b6301e7a5c8bb2a6
damir <damir.say80@gmail.com>
2024-12-06 01:27:33 +0700

    sddddddddddddd

--- client/src/shared/http/index.ts
+++ client/src/shared/http/index.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import { AuthResponse } from '../types';
 
+const API_URL = `http://79.141.65.250:3001/api`;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
 const refreshToken = async (): Promise<AuthResponse> => {
   try {
-    const response = await axios.get<AuthResponse>(`/api/refresh`);
+    const response = await api.get<AuthResponse>(`/refresh`);
     const data = response.data;
 
     localStorage.setItem(
