44b9cb1fb51ad63df3499ea6f545e8eca2595298
damir <damir.say80@gmail.com>
2024-12-06 02:29:59 +0700

    d

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,11 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig,} from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(() => {
 
   return {
     plugins: [react()],
-    server: {
-      port: +env.VITE_CLIENT_PORT,
-      host: true,
-      watch: {
-        usePolling: true,
-      },
-      esbuild: {
-        target: 'esnext',
-        platform: 'browser',
-      },
-    },
-    define: {
-      VITE_SERVER_PORT: JSON.stringify(env.VITE_SERVER_PORT),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'), // Используйте path.resolve для корректного разрешения
